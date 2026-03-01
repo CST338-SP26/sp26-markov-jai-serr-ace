b33d7e6a5105c94d9dbe7d462f2da5f8bb4ddb4a
Jaime Serrano Acevedo <xilverscott@gmail.com>
2026-02-28 20:42:43 -0800

    Project has been debugged and passes all the cases

--- src/main/java/Markov.java
+++ src/main/java/Markov.java
@@ -7,18 +7,22 @@ import java.io.FileNotFoundException;
 import java.util.Random;
 
 public class Markov {
-    private static String BEGINS_SENTENCE;
+    private static final String BEGINS_SENTENCE = "__$";
     private String prevWord;
-    private HashMap<String, ArrayList<String>> words;
-    private static String PUNCTUATION_MARKS;
+    private final HashMap<String, ArrayList<String>> words = new HashMap<>();
+    private static final String PUNCTUATION_MARKS = ".!?$";
 
-    public Markov() {}
+    public Markov() {
+        ArrayList<String> temp = new ArrayList<>();
+        words.put(BEGINS_SENTENCE,temp);
+        prevWord = BEGINS_SENTENCE;
+    }
 
     public String getSentence(){
-
         StringBuilder sentence = new StringBuilder();
+        String current_word = BEGINS_SENTENCE;
         while(true) {
-            String temp = randomWord(BEGINS_SENTENCE);
+            String temp = randomWord(current_word);
             if (!endsWithPunctuation(temp)) {
                 sentence.append(temp);
                 sentence.append(" ");
@@ -27,8 +31,8 @@ public class Markov {
                 sentence.append(temp);
                 break;
             }
+            current_word = temp;
         }
-
         return sentence.toString();
     }
 
@@ -40,29 +44,32 @@ public class Markov {
             }
         }
         catch (FileNotFoundException e){
-            System.out.println("Error has occured");
+            System.out.println("Error has occurred");
         }
 
     }
 
     void addWord(String word){
+        if(word.isEmpty()){
+            return;
+        }
         if(endsWithPunctuation(prevWord)){
             words.get(BEGINS_SENTENCE).add(word);
         }
         else {
             if(!words.containsKey(prevWord)){
                 //adding word to key
-                words.put(prevWord,null);
+                ArrayList<String> empt = new ArrayList<>();
+                words.put(prevWord,empt);
             }
-            else {
-                words.get(prevWord).add(word);
-            }
+            words.get(prevWord).add(word);
         }
+        prevWord = word;
     }
 
     String randomWord(String rand_word) {
         Random random = new Random();
-        int rand_int = random.nextInt(words.get(rand_word).size()-1);
+        int rand_int = random.nextInt(words.get(rand_word).size());
         return words.get(rand_word).get(rand_int);
     }
 
@@ -76,14 +83,14 @@ public class Markov {
 
     void addLine(String line){
         if(!line.isEmpty()) {
-            String[] temp = line.split(" ");
+            String[] temp = line.trim().split("[ \n\t]");
             for(String a : temp){
                 addWord(a);
             }
         }
     }
 
-    public boolean endsWithPunctuation(String phrase){
+    public static boolean endsWithPunctuation(String phrase){
         String temp = phrase.substring(phrase.length() - 1);
         return PUNCTUATION_MARKS.contains(temp);
     }
